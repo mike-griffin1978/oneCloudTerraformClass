0259da8db9330ecd8ebe93a5e20594354d441174
Scott Carruthers <Iilluminorowebdev@gmail.com>
2021-06-14 22:34:48 -0400

    edits to lab 12

--- lab-12-local-variables/section-02-local-values-example/resources.tf
+++ lab-12-local-variables/section-02-local-values-example/resources.tf
@@ -1,6 +1,6 @@
 # Terraform Block
 terraform {
-  required_version = "~> 0.15.4" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
+  required_version = "~> 1.0.0" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -15,14 +15,21 @@ provider "aws" {
   profile = "default"
 }
 
-####Note regarding lab usage of and the local values blocks#########
+####Note regarding lab usage and the Resource blocks#########
 ####Replace all references to lab Pod Number 0 with your unqiue, assigned pod number
-####Example - bucket-0 - replace  the key value with your own pod number such
-#### as (bucket-1)
+####Example - bucket-0 - replace the key value with your own pod number such
+#### as "bucket-1" for student pod #1
 ####If preferred and for expidency/precision - considering doing a replace all such as - replace all 
 ####occurances of -0 with -1
 
 # Define Local Values
+# The bucket name is dervived from the syntax of - <app_name_from_variables_file> + "-" 
+# + <env_name_from_variables_file> + "-bucket-<pod-number>"
+# To avoid duplication of complex logic - for cleaner code and to be less error prone - the complex
+# statement is stored as a Local Value/variable and is then subsequently called as the bucket name and
+# as the tag Name value
+# An additional benefit of the Local Value strategy is that any future desire to update the naming scheme can
+# be conducted thru an edit/change in a single variable and not in several places within the code
 locals {
   bucket-name = "${var.app_name}-${var.environment_name}-bucket-0" # Complex expression
 }
