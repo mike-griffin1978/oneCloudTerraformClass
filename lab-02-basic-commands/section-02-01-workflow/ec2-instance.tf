bf84b1b3f241e9c4c99728b385d6a0a40b1bc061
actswartz <j@nr4.com>
2023-05-31 12:16:26 -0400

    Update ec2-instance.tf

--- lab-02-basic-commands/section-02-01-workflow/ec2-instance.tf
+++ lab-02-basic-commands/section-02-01-workflow/ec2-instance.tf
@@ -16,13 +16,13 @@ provider "aws" {
 ####Note regarding lab usage of and the Resource blocks#########
 ####Replace all references to lab Pod Number 0 with your unqiue, assigned pod number
 ####Example - ec2-instance-0 within the tags block - replace the tags Name value to include 
-### your own pod number such as - "ec2-instance-<pod-number>"
+### your own pod number such as - "ec2-instance-Pod<pod-number>"
 
 # Resource Block
 resource "aws_instance" "ec2demo" {
   ami           = "ami-0742b4e673072066f" # Amazon Linux in us-east-1, update as per your region
   instance_type = "t2.micro"
   tags = {
-    "Name" = "ec2-instance-0"
+    "Name" = "ec2-instance-Pod0"
   }
 }
